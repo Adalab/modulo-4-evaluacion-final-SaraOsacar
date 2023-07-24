1ba6737b9278df01cc096c768d9b2641e1855625
SaraOsacar <saraosacar@gmail.com>
2023-07-24 12:35:08 +0200

    add sql file

---
+++ .sql
@@ -0,0 +1,12 @@
+CREATE DATABASE recetas_db;
+USE recetas_db;
+
+CREATE TABLE recetas (
+id int auto_increment primary key,
+nombre varchar(45),
+ingredientes varchar (100),
+instrucciones text
+
+);
+
+
